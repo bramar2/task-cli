2b160606f74ec22ccfcf94032083d73ffc485430
bramar2 <bramar224@gmail.com>
2025-03-27 00:57:08 +0700

    update help

--- README.md
+++ README.md
@@ -17,10 +17,10 @@ Task CLI --- A CLI tool to manage all your tasks | github.com/bramar2/task-cli
 | task-cli update [ID] "New desc"               - Update task description.
 | task-cli delete [ID]                          - Delete a task.
 | task-cli mark [ID] [todo/in-progress/done]    - Mark task status.
-| task-cli list                                 - Displays a list of all tasks.
-| task-cli list [todo/in-progress/done]         - Displays a list of all tasks with the specified status.
+| task-cli list                                 - Display a list of all tasks.
+| task-cli list [todo/in-progress/done]         - Display a list of all tasks with the specified status.
 | task-cli terminal                             - Toggles terminal mode for quick task managing.
 | task-cli exit                                 - Exit.
-| task-cli help                                 - Shows this help menu.
+| task-cli help                                 - Show this help menu.
 ```
 ![image](https://github.com/user-attachments/assets/32684c85-1ca5-4691-bcc8-b561a24ee187)
--- program.cpp
+++ program.cpp
@@ -19,15 +19,15 @@
 
 constexpr char HELP[] = 
 R"(Task CLI --- A CLI tool to manage all your tasks | github.com/bramar2/task-cli
-| task-cli add "Task description"		- Add a task and returns the Task ID.
-| task-cli update [ID] "New desc"		- Update task description.
-| task-cli delete [ID]				- Delete a task.
-| task-cli mark [ID] [todo/in-progress/done]	- Mark task status.
-| task-cli list					- Displays a list of all tasks.
-| task-cli list [todo/in-progress/done]		- Displays a list of all tasks with the specified status.
-| task-cli terminal				- Toggles terminal mode for quick task managing.
-| task-cli exit					- Exit.
-| task-cli help					- Shows this help menu.
+| task-cli add "Task description"               - Add a task and returns the Task ID.
+| task-cli update [ID] "New desc"               - Update task description.
+| task-cli delete [ID]                          - Delete a task.
+| task-cli mark [ID] [todo/in-progress/done]    - Mark task status.
+| task-cli list                                 - Display a list of all tasks.
+| task-cli list [todo/in-progress/done]         - Display a list of all tasks with the specified status.
+| task-cli terminal                             - Toggles terminal mode for quick task managing.
+| task-cli exit                                 - Exit.
+| task-cli help                                 - Show this help menu.
 )";
 
 constexpr char DATA_FILE[] = "task-cli.json";
